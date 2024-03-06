cc6ce874d2099b14c502dd809c0ca845cbf590f6
Chiocheti <gabrielchiocheti@gmail.com>
2024-03-05 21:51:42 -0300

    chore(button): set border-color

--- src/components/default/Button.jsx
+++ src/components/default/Button.jsx
@@ -2,7 +2,13 @@ import MuiButton from '@mui/material/Button';
 
 export default function Button({ text, color, ...rest }) {
   return (
-    <MuiButton sx={{ height: '100%' }} variant="outlined" color={color} fullWidth {...rest}>
+    <MuiButton
+      sx={{ height: '100%', borderColor: 'primary.contrastText' }}
+      variant="outlined"
+      color={color}
+      fullWidth
+      {...rest}
+    >
       {text}
     </MuiButton>
   );
